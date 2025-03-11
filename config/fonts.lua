43ae77253e25ebc3908ebf4d20fafbcd06a2a095
Szabolcs Seláf <selu@selu.org>
2025-03-11 15:34:08 +0100

    add unifont as a fallback

--- config/fonts.lua
+++ config/fonts.lua
@@ -2,14 +2,17 @@ local wezterm = require('wezterm')
 local platform = require('utils.platform')
 
 -- local font = 'Maple Mono SC NF'
-local font_family = 'JetBrainsMono Nerd Font'
-local font_size = platform.is_mac and 12 or 9
+local font_family = 'CaskaydiaCove Nerd Font Mono'
+local font_size = platform.is_mac and 12 or 10
 
 return {
-   font = wezterm.font({
+   font = wezterm.font_with_fallback({{
       family = font_family,
+      weight = 'Regular',
+   }, {
+      family = 'Unifont',
       weight = 'Medium',
-   }),
+   }}),
    font_size = font_size,
 
    --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
